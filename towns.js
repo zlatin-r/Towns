e3d5b760dfb69eb7c227b3d503dffb27a12645b8
rusev-z <rusev.dev@gmail.com>
2025-01-28 15:24:51 +0100

    Update towns.js
    added addTown function by the editor

--- towns.js
+++ towns.js
@@ -17,3 +17,10 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+
+function addTown() {
+    let townName = $('#townNameForAdd').val();
+    $('#townNameForAdd').val('');
+    $('#towns').append($('<option>').text(townName));
+    $('#result').text(townName + " added.");
+}
